ba2ae3f47f8d22da9508415afaba03b950790e8c
kkriystal <80847567+kkriystal@users.noreply.github.com>
2022-03-20 05:16:49 +0800

    Add files via upload

---
+++ addresses/projectTwo.js
@@ -0,0 +1,8 @@
+const projectTwo_addresses = {
+    contract: "0x022b0f871eDc12B1549f894e3A2d721BBEf7519b",
+    team_accounts: ["0x0000000000000000000000000000000000000000",
+    "0x0000000000000000000000000000000000000000",
+  ]
+}
+
+module.exports = {projectTwo_addresses}
