54aff9b9426632f1d02f98399d7f8902ae65e48a
kkriystal <80847567+kkriystal@users.noreply.github.com>
2023-11-13 11:42:40 +0800

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -41,7 +41,7 @@ updateprojectOneData = async (chainData, client) => {
 updateTipsyCoin = async (chainData, client) => {
     try {
         const database = client.db('fomo')
-        const collection = database.collection('tipsy')
+        const collection = database.collection('judas')
         chainData.timestamp = Date.now()
         let newChainData = Object.assign({}, chainData)
         newChainData._id = ObjectId()
@@ -103,7 +103,7 @@ getCachedprojectTwoData = async (client) => {
 getCachedTipsyData = async (client) => {
     try {
         const database = client.db('fomo')
-        const collection = database.collection('tipsy')
+        const collection = database.collection('judas')
         cachedData = await collection.find().sort({ _id: -1 }).limit(1).toArray()
         cachedData = cachedData[0]
         return cachedData    
