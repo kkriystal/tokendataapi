70c02ea83df49aa1a7b5e97b4b2e9e03fe10c118
kkriystal <80847567+kkriystal@users.noreply.github.com>
2023-11-13 11:00:47 +0800

    Update judas.js

--- addresses/judas.js
+++ addresses/judas.js
@@ -1,8 +1,8 @@
-const tipsy_address = {
+const judas_address = {
     contract: "0xA5e0B9118cE23DD1eec52CD93Cf59e8EA3b3Ff56",
     team_accounts: ["0x0000000000000000000000000000000000000000",
     "0x0000000000000000000000000000000000000000",
   ]
 }
 
-module.exports = {tipsy_address}
+module.exports = {judas_address}
